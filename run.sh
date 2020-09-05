374434ca6621a039f7b473f7443f1510d113296b
Jordan Sokolic <oofnik@gmail.com>
2020-09-05 16:05:32 +0300

    bind mount fw extension

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ set -e
 DEVICE=$(lsusb | grep 'Fingerprint' | awk -F '[^0-9]+' '{ print "/dev/bus/usb/" $2 "/" $3 }')
 PWD=$(pwd)
 BASENAME=$(basename "$PWD")
+XPFWEXT=$(ls ./*.xpfwext)
 
 docker run --rm -it \
     --device $DEVICE \
     --env-file .env \
     -v $PWD:/$BASENAME \
     -v $PWD/usb.txt:/root/.wine/drive_c/usb.txt \
+    -v $PWD/$XPFWEXT:/root/.wine/drive_c/system32/$XPFWEXT \
     wine:validity wine64 "/${BASENAME}/a.exe" "$@" | tee "$(date +%s)".log
