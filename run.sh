3eebad64f04b1d0c6cef25458c5f29d92f4bd572
Viktor Dragomiretskyy <Viktor.Dragomiretskyy@gmail.com>
2022-05-05 17:31:52 +1200

    Split the usb replay, wine and stdout logs into separate files.

--- run.sh
+++ run.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
 set -e
 
-DEVICE=$(lsusb | grep 'Fingerprint' | awk -F '[^0-9]+' '{ print "/dev/bus/usb/" $2 "/" $3 }')
+DEVICE=$(lsusb | grep 'Validity' | awk -F '[^0-9]+' '{ print "/dev/bus/usb/" $2 "/" $3 }')
 PWD=$(pwd)
 BASENAME=$(basename "$PWD")
 XPFWEXT=$(ls ./*.xpfwext)
+NOW=$(date +%s)
+
+SCRIPT="${NOW}-usb.txt"
+PLAYBACK=
+
+while getopts "p:" arg; do
+    case $arg in
+        p) 
+            SCRIPT=$OPTARG
+            PLAYBACK=1
+            ;;
+        *)
+            echo "Usage: $0 [-p usb-script.txt] <nop|enroll|identify>"
+            exit 0
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+WHAT=$1
 
 docker run --rm -it \
     --device $DEVICE \
     --env-file .env \
+    -w /$BASENAME \
+    -e USB_SCRIPT=$SCRIPT \
+    -e USB_PLAYBACK=$PLAYBACK \
     -v $PWD:/$BASENAME \
     -v $PWD/usb.txt:/root/.wine/drive_c/usb.txt \
     -v $PWD/$XPFWEXT:/root/.wine/drive_c/system32/$XPFWEXT \
-    wine:validity wine64 "/${BASENAME}/a.exe" "$@" | tee "$(date +%s)".log
+    wine:validity sh -c "wine64 a.exe $WHAT 2> ${NOW}-wine.log" | tee "${NOW}.log"
